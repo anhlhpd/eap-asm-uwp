0e5a393c1d5d187745e661d0d8dcbe419c8a2963
anhlhpd00579 <anhlhpd00579@fpt.edu.vn>
2018-12-31 12:13:44 +0700

    tạo view login và thông tin cá nhân;

---
+++ Client/Views/NavigationView.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Client.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class NavigationView : Page
+    {
+        private Type currentPage;
+        
+        private readonly List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
+            {
+                ("MyAccount", typeof(MyAccount))
+            };
+
+
+        public NavigationView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            NavView_Navigate("MyAccount");
+            NavView.SelectedItem = NavView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(n => n.Tag.Equals("MyAccount"));
+            NavView.IsPaneOpen = false;
+
+            var altLeft = new KeyboardAccelerator
+            {
+                Key = VirtualKey.Left,
+                Modifiers = VirtualKeyModifiers.Menu
+            };
+            this.KeyboardAccelerators.Add(altLeft);
+        }
+
+
+        private void NavView_ItemInvoked(Windows.UI.Xaml.Controls.NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.InvokedItem == null)
+                return;
+
+            if (args.IsSettingsInvoked)
+                ContentFrame.Navigate(typeof(SettingsPage));
+            else
+            {
+                // Getting the Tag from Content (args.InvokedItem is the content of NavigationViewItem)
+                var navItemTag = NavView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(i => args.InvokedItem.Equals(i.Content))
+                    .Tag.ToString();
+
+                NavView_Navigate(navItemTag);
+            }
+        }
+
+        private void NavView_Navigate(string navItemTag)
+        {
+            var item = _pages.First(p => p.Tag.Equals(navItemTag));
+            if (currentPage == item.Page)
+                return;
+            ContentFrame.Navigate(item.Page);
+
+            currentPage = item.Page;
+        }
+        
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            //NavView.IsBackEnabled = ContentFrame.CanGoBack;
+            //Debug.WriteLine(ContentFrame.SourcePageType);
+            if (ContentFrame.SourcePageType == typeof(SettingsPage))
+            {
+                // SettingsItem is not part of NavView.MenuItems, and doesn't have a Tag
+                NavView.SelectedItem = (NavigationViewItem)NavView.SettingsItem;
+            }
+            else
+            {
+                var item = _pages.First(p => p.Page == e.SourcePageType);
+
+                NavView.SelectedItem = NavView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(n => n.Tag.Equals(item.Tag));
+            }
+        }
+    }
+}
